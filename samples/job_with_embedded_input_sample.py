5b912763ec71a90bdd83d040a31cacafbc3ef27a
Raul Casallas <raul.casallas@bairesdev.com>
2020-08-28 11:57:21 -0500

    fix: improvements on samples

--- samples/job_with_embedded_input_sample.py
+++ samples/job_with_embedded_input_sample.py
@@ -1,63 +1,120 @@
+import json
 import sys
 import os
 import logging
 import dotenv
+
+from modzy.error import ResultsError
+from modzy.jobs import Jobs
+
 sys.path.insert(0, '..')
 from modzy import ApiClient
 from modzy._util import file_to_bytes
 
 
-# Basic configuration of logger (level setted in debug)
-
-logging.basicConfig(level=logging.DEBUG)
+# Always configure the logger level (ie: DEBUG, INFO, WARNING, ERROR, CRITICAL)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# dotenv variables
+# The system admin can provide the right base API URL, the API key can be downloaded from your profile page on Modzy.
+# You can config those params as is described in the readme file (as environment variables, or by using the .env file),
+# or you can just set BASE_URL and API_KEY vars on this sample code (not recommended for production environments).
 
 dotenv.load_dotenv()
 
+# The MODZY_BASE_URL should point to the API services route, it may be different from the Modzy page URL.
+# (ie: https://modzy.example.com/api).
 BASE_URL = os.getenv('MODZY_BASE_URL')
+# The MODZY_API_KEY is your own personal API key. It is composed by a public part, a dot character and a private part
+# (ie: AzQBJ3h4B1z60xNmhAJF.uQyQh8putLIRDi1nOldh).
 API_KEY = os.getenv('MODZY_API_KEY')
 
 # Client initialization
-# TODO: set the base url of modzy api and you api key
+#   Initialize the ApiClient instance with the BASE_URL and the API_KEY to store those arguments
+#   for the following API calls.
 client = ApiClient(base_url=BASE_URL, api_key=API_KEY)
 
-# Create a Job with a text input, wait and retrieve results
-image_bytes = file_to_bytes('../samples/image.png')
+# Create a Job with a embedded input, wait and retrieve results
 
-model = client.models.get_by_name("NSFW Image Detection")
-logger.info("Model {}".format(model))
+# Get the model object:
+# If you already know the model identifier (i.e.: you get from the URL of the model details page or the input sample),
+# you can skip this step, if you don't you can find the model identifier by using its name as follows:
+model = client.models.get_by_name("Multi-Language OCR")
+# Or if you already know the model id and want to know more about the model, you can use this instead:
+# model = client.models.get("c60c8dbd79")
+# You can find more information about how to query the models on the model_sample.py file
 
+# The model identifier is under the modelId key. You can take a look at the other keys by uncommenting the following line
+# logger.info(", ".join("{} :: {}".format(key, value) for key, value in model.items()))
+# Or just log the model identifier, and potencially the latest version
+logger.info("The model identifier is {} and the latest version is {}".format(model.modelId, model.latest_version))
+
+# Get the model version object:
+# If you already know the model version and the input key(s) of the model version you can skip this step. Also, you can
+# use the following code block to know about the inputs keys, and skip the call on future job submissions.
 modelVersion = client.models.get_version(model, model.latest_version)
-logger.info("ModelVersion {}".format(modelVersion))
+# The info stored in modelVersion provides insights about the amount of time that the model can spend processing,
+# the inputs, and output keys of the model.
+logger.info("Ths model version is {}".format(modelVersion))
+logger.info("  timeouts: status {}ms, run {}ms ".format(modelVersion.timeout.status, modelVersion.timeout.run))
+logger.info("  inputs: ")
+for input in modelVersion.inputs:
+    logger.info("    key {}, type {}, description: {}".format(input.name, input.acceptedMediaTypes, input.description))
+logger.info("  outputs: ")
+for output in modelVersion.outputs:
+    logger.info("    key {}, type {}, description: {}".format(output.name, output.mediaType, output.description))
 
-job = client.jobs.submit_bytes(model.modelId, modelVersion.version, {'image': image_bytes})
+# Send the job:
+# An embedded input is a byte array encoded as a string in Base64, that's very handy for small to middle size files, for
+# bigger files can be a memory issue because you need to load the file in memory (load + encode).
+image_bytes = file_to_bytes('../samples/image.png')
+config_bytes = file_to_bytes('../samples/config.json')
+# With the info about the model (identifier), the model version (version string, input/output keys), you are ready to
+# submit the job. Just prepare the source dictionary:
+sources = {"source-key": {"input": image_bytes, "config.json":config_bytes}}
+# An inference job groups input data that you send to a model. You can send any amount of inputs to
+# process and you can identify and refer to a specific input by the key that you assign, for example we can add:
+sources["second-key"] = {"input": image_bytes, "config.json":config_bytes}
+# You don't need to load all the inputs from files, just convert to bytes as follows:
+config_bytes = json.dumps({"languages":["spa"]}).encode('utf-8')
+sources["another-key"] = {"input": image_bytes, "config.json":config_bytes}
+# If you send a wrong input key, the model fails to process the input.
+sources["wrong-key"] = {"a.wrong.key": image_bytes, "config.json":config_bytes}
+# If you send a correct input key, but some wrong values, the model fails to process the input.
+sources["wrong-value"] = {"input": config_bytes, "config.json":image_bytes}
+# When you have all your inputs ready, you can use our helper method to submit the job as follows:
+job = client.jobs.submit_bytes_bulk(model.modelId, modelVersion.version, sources)
+# Modzy creates the job and queue for processing. The job object contains all the info that you need to keep track
+# of the process, the most important being the job_identifier and the job status.
 logger.info("job: %s", job)
-
+# The job moves to SUBMITTED, meaning that Modzy acknowledged the job and sent it to the queue to be processed.
+# We provide a helper method to hold until the job finishes processing. Its a good practice to set a max timeout
+# if you're doing a test (ie: 2*status+run). Otherwise, if the timeout is set to None, it will hold until the job
+# finishes and moves to COMPLETED, CANCELED, or TIMEOUT.
 job.block_until_complete(timeout=None)
-logger.info("job: %s", job)
 
-result = job.get_result()
-logger.info("result: %s", result)
+# Get the results:
+# Check the status of the job. Jobs may be canceled or can reach a timeout.
+if job.status == Jobs.status.COMPLETED:
+    # A completed job means that all the inputs were processed by the model. Check the results for each
+    # input key provided in the source dictionary to see the model output.
+    result = job.get_result()
+    # The result object has some useful info:
+    logger.info("Result: finished: {}, total: {}, completed: {}, failed: {}"
+                .format(result.finished, result.total, result.completed, result.failed))
+    # Notice that we are iterating thought the same keys of the input sources
+    for key in sources:
+        # The result object has the individual results of each job input. In this case the output key is called
+        # results.json, so we can get that specific model result as follows:
+        try:
+            model_res = result.get_source_outputs(key)['results.json']
+            # The output for this model comes in a JSON format, so we can directly log the model results:
+            logger.info(
+                "    {}: ".format(key) + ", ".join(
+                    "{}: {}".format(key, str(value).replace('\n', '')) for key, value in model_res.items()))
+        except ResultsError as failure:
+            # If the model raises an error, we can get the specific error message:
+            logger.warning("    {}: failure: {}".format(key, failure));
 
-results_json = result.get_first_outputs()['results.json']
-logger.info("results_json: %s", results_json)
-
-# Create a job with more than one input
-
-job = client.jobs.submit_bytes_bulk(model.modelId, modelVersion.version, {
-        'image-1': {'image': image_bytes},
-        'other-id': {'image': image_bytes},
-})
-
-logger.info("job: %s", job)
-
-job.block_until_complete(timeout=None)
-logger.info("job: %s", job)
-
-result = job.get_result()
-logger.info("result: %s", result)
-
-results_json = result.get_first_outputs()['results.json']
-logger.info("results_json: %s", results_json)
+else:
+    logger.warning("The job ends with status {}".format(job))
--- samples/model_sample.py
+++ samples/model_sample.py
@@ -6,48 +6,82 @@ sys.path.insert(0, '..')
 from modzy import ApiClient
 
 
-# Basic configuration of logger (level setted in debug)
-
-logging.basicConfig(level=logging.DEBUG)
+# Always configure the logger level (ie: DEBUG, INFO, WARNING, ERROR, CRITICAL)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# dotenv variables
+# The system admin can provide the right base API URL, the API key can be downloaded from your profile page on Modzy.
+# You can config those params as is described in the readme file (as environment variables, or by using the .env file),
+# or you can just set BASE_URL and API_KEY vars on this sample code (not recommended for production environments).
 
 dotenv.load_dotenv()
 
+# The MODZY_BASE_URL should point to the API services route, it may be different from the Modzy page URL.
+# (ie: https://modzy.example.com/api).
 BASE_URL = os.getenv('MODZY_BASE_URL')
+# The MODZY_API_KEY is your own personal API key. It is composed by a public part, a dot character and a private part
+# (ie: AzQBJ3h4B1z60xNmhAJF.uQyQh8putLIRDi1nOldh).
 API_KEY = os.getenv('MODZY_API_KEY')
 
 # Client initialization
-# TODO: set the base url of modzy api and you api key
+#   Initialize the ApiClient instance with the BASE_URL and the API_KEY to store those arguments
+#   for the following API calls.
 client = ApiClient(base_url=BASE_URL, api_key=API_KEY)
 
 # Get all models
-
+# You can get the full list of models from Modzy by using get_all method, this call will return just the identifier
+# and the latest version of each model
 models = client.models.get_all()
-
 logger.info("all models: %s", len(models))
+# Also, you can do more interesting search by the get_models method:
+# Search by author
+params = {'author': 'Open Source'}
+models = client.models.get_models(**params)
+logger.info("Open Source models: %s", len(models))
+# Active models
+params = {'is_active': True}
+models = client.models.get_models(**params)
+logger.info("Active models: %s", len(models))
+# Search by name (and limiting the results)
+params = {'name': "Image", 'per_page':5}
+models = client.models.get_models(**params)
+logger.info("Models with name start with 'Image': %s", len(models))
+# Combined search
+params = {'name': "Image", 'author': 'Open Source', 'is_active': True}
+models = client.models.get_models(**params)
+logger.info("Active open source models which name starts with 'Image': %s", len(models))
+# Get model details
+# the models route didn't return much info about the models, just modelId, latestVersion and versions:
 for model in models:
     logger.info(", ".join("{} :: {}".format(key, value) for key, value in model.items()))
+    # In order to get more info about the models you need to get the details by identifier
+    model = client.models.get(model.modelId)
+    # then you'll get all the details about the model
+    logger.info("Model detail keys: %s", model.keys())
+    # In order to get information about the input/output keys and types you need to get the model version details as
+    # follows:
+    modelVersion = client.models.get_version(model, model.latestVersion)
+    # then you'll get all the details about the specific model version
+    logger.info("Model Version detail keys: %s", model.keys())
+    # Probably the more interesting are the ones related with the inputs and outputs of the model
+    logger.info("  inputs: ")
+    for input in modelVersion.inputs:
+        logger.info(
+            "    key {}, type {}, description: {}".format(input.name, input.acceptedMediaTypes, input.description))
+    logger.info("  outputs: ")
+    for output in modelVersion.outputs:
+        logger.info("    key {}, type {}, description: {}".format(output.name, output.mediaType, output.description.replace('\n', '')))
 
-
-# Get model by identifier
-logger.info("model by id ed542963de")
-model = client.models.get('ed542963de')  # sentiment-analysis
-logger.info("When you get the model by id, you get the following model keys: %s", model.keys())
-
-
-# Get related models
-
-logger.info("related models by id ed542963de")
-models = client.models.get_related('ed542963de')
+# Get model by name
+# If you aren't familiar with the models ids, you can find the model by name as follows
+model = client.models.get_by_name("Dataset Joining")
+# the method will return the first coincidence and return the details
+logger.info("Dataset Joining: id:%s, author: %s, is_active: %s, description: %s",
+            model.modelId, model.author, model.is_active, model.description)
+# Finally, you can find the models related with this search
+models = client.models.get_related(model.modelId)
+logger.info("related models")
 for model in models:
-    logger.info("related model %s", model)
-
-# Get versions of a model
-
-versions = client.models.get_versions('ed542963de')
-for version in versions:
-    logger.info("version %s", version)
+    logger.info("    %s :: %s (%s)", model.modelId, model.name, model.author)
 
 
