09c5aecb77f9078916be3fc144128ddb14e5a43f
Raul Casallas <raul.casallas@bairesdev.com>
2020-12-07 14:21:51 -0500

    chores: up version

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/modzy/sdk-python',
-    version='0.5.1',
+    version='0.5.2',
     zip_safe=False,
 )
