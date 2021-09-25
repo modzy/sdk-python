3a8e25376f12cb1c7ef633d2169fb827ff1a158a
Saumil Dave <dave_saumil@bah.com>
2021-09-25 14:51:25 -0700

    refactor: generalize model dir upload helper

--- modzy/converter/utils.py
+++ modzy/converter/utils.py
@@ -67,3 +67,48 @@ def upload_resources(model_yaml_path, container, resources_key,
 
     # Remove temp dir
     shutil.rmtree(tmp_dir_path)
+
+
+def upload_model_dir(model_dir, container, model_key,
+                        storage_key, storage_secret, storage_provider):
+    """ Creates resources archive expected by model converter, uploads to storage provider.
+    Args:
+        model_dir (str): Path to saved model directory 
+        container (str): Storage provider container name (e.g. Bucket name in S3).
+        resources_key (str): Desired key for model archive once uploaded to storage provider.
+        storage_key (str): Storage provider access key.
+        storage_secret (str): Storage provider secret key.
+        storage_provider (str): Storage provider name (must be one of "S3", "AZURE_BLOBS", or "GOOGLE_STORAGE").
+    """
+    driver = get_authenticated_storage_provider_client(storage_provider, storage_key, storage_secret)
+    container = driver.get_container(container_name=container)
+
+    # TODO: Probably set this outside of this helper function
+    MODEL_TAR_NAME = "weights.tar.gz"
+
+    # Create temp dir
+    tmp_dir_path = os.path.join(os.getcwd(), ".tmp_" + str(time.time()))
+    os.mkdir(tmp_dir_path)
+
+    # Move the local model artifacts into an archive
+    model_tar_path = os.path.join(tmp_dir_path, MODEL_TAR_NAME)
+    tar = tarfile.open(model_tar_path, "w:gz")
+    model_filenames = os.listdir(model_dir)
+    for filename in model_filenames:
+        if not filename.startswith('.'):
+            full_path = os.path.join(model_dir, filename)
+            tar.add(full_path, arcname=os.path.join('imagefiles/',filename))
+    tar.close()
+
+    # This method blocks until all the parts have been uploaded.
+    extra = {'content_type': 'application/octet-stream'}
+
+    # Upload archive to storage provider
+    with open(model_tar_path, 'rb') as iterator:
+        obj = driver.upload_object_via_stream(iterator=iterator,
+                                              container=container,
+                                              object_name=model_key,
+                                              extra=extra)
+
+    # Remove temp dir
+    shutil.rmtree(tmp_dir_path)
