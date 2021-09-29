bc286aca4a42061e99fa157656ab169347ccfed7
Raul Casallas <raul.casallas@bairesdev.com>
2021-09-29 07:23:25 -0500

    add deprecated

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['requests', 'python-dotenv']
+requirements = ['requests', 'python-dotenv', 'deprecated']
 
 test_requirements = ['pytest']
 
