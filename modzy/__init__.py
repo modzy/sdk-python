89f966fe4c252da17e758217bf8b2508e52f389f
Bradley Munday <bradley.munday@modzy.com>
2023-01-03 20:56:56 -0500

    Added inference API support with edge client

--- modzy/__init__.py
+++ modzy/__init__.py
@@ -5,6 +5,6 @@ import logging
 
 from .client import ApiClient  # noqa
 from .edge.client import EdgeClient
-__version__ = '0.10.4'
+__version__ = '0.11.0'
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
