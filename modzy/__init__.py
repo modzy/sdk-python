f8d551c87b9d49bad52e42974d10b1abae80786d
Bradley Munday <bradley.munday@modzy.com>
2023-02-14 18:06:53 -0500

    Added .pyi to package data

--- modzy/__init__.py
+++ modzy/__init__.py
@@ -5,6 +5,6 @@ import logging
 
 from .client import ApiClient  # noqa
 from .edge.client import EdgeClient
-__version__ = '0.11.2'
+__version__ = '0.11.3'
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
